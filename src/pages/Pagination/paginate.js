16dad329363d528e20e514e61dd1ffe071332b66
jdotcole233 <jdotcole233@gmail.com>
2021-07-01 23:49:49 +0000

    pagination

--- src/pages/Pagination/paginate.js
+++ src/pages/Pagination/paginate.js
@@ -1,20 +1,25 @@
-import './paginate.css';
+import { useState } from 'react';
+import './paginate.css'; 
 
 const Paginate = ({ data, pagesize }) => {
 
+    const [tilePosition, setTilePosition] = useState(pagesize)
+
     const numberOfTiles = data.length / pagesize;
     const tiles = [];
-    const mult = 0;
+    let mult = 0;
     for (let i = 0; i < Math.ceil(numberOfTiles); i++) {
-        tiles.push(mult + pagesize);
+        mult += pagesize;
+        tiles.push(mult);
     }
-
 
     return (
       <div className="paginateTiles">
         <ul>
           {tiles.map((tile, key) => (
-            <li id="">{key + 1}</li>
+            <li key={key} onClick={() => setTilePosition(tile)}>
+              {key + 1}
+            </li>
           ))}
         </ul>
       </div>
